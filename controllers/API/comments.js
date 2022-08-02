fa1f90dcecddfad3260113c8b5f0675e309a9441
Matt F <fiaschettima@gmail.com>
2022-08-02 01:36:23 -0400

    add skeleton routes for comments

--- controllers/API/comments.js
+++ controllers/API/comments.js
@@ -1,1 +1,32 @@
 //routes to add / delete comments
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// add comment route
+router.post('/', async (req,res) => {
+    try{
+        const newComment = await Comment.create({
+            content: req.body.content,
+            // user_id: ? get from somewhere besides body
+            //post_id: same here find where to pull id from
+        });
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+// update comment route
+router.put('/', async (req,res) => {
+    try{
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+// delete comment route
+router.delete('/', async (req,res) => {
+    try{
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
