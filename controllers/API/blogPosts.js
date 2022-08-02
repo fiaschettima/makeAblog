5ec63a9380f17dd8edb296ce69a8dfaa00c8b107
Matt F <fiaschettima@gmail.com>
2022-08-02 02:14:57 -0400

    all blog posts routes filled out

--- controllers/API/blogPosts.js
+++ controllers/API/blogPosts.js
@@ -1,1 +1,88 @@
 // routes to update create and delet posts
+const router = require('express').Router();
+const { BlogPost } = require('../../models')
+
+// get all posts
+router.get('/', async (req,res) => {
+    try{
+        const postData = await BlogPost.findAll()
+        if(!postData){
+            res.status(404).json({message: 'Theres No Posts'})
+            return;
+        }
+        res.status(200).json(postData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+// get one post
+router.get('/:id', async (req,res) => {
+    try{
+        const postData = await BlogPost.findByPk({
+            where:{
+                id: req.params.id
+            }
+        })
+        if(!postData){
+            res.status(404).json({message: 'Cant find this Posts'})
+            return;
+        }
+        res.status(200).json(postData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+// add post
+router.post('/', async (req,res) => {
+    try{
+        const postData = await BlogPost.create({
+                user_id: req.session.user_id,
+                post_title: req.body.post_title,
+                post_content: req.body.post_content
+            }
+        )
+        if(!postData){
+            res.status(404).json({message: 'Theres No Posts'})
+            return;
+        }
+        res.status(200).json(postData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+// update post
+router.get('/:id', async (req,res) => {
+    try{
+        const postData = await BlogPost.update(
+            {     post_title: req.body.post_title,
+                post_content: req.body.post_content
+            },
+            {where:{id: req.params.id}}
+        )
+        if(!postData){
+            res.status(404).json({message: 'Post not found'})
+            return;
+        }
+        res.status(200).json(postData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+// delete post
+router.get('/:id', async (req,res) => {
+    try{
+        const postData = await BlogPost.findByPk({
+            where: {
+                id: req.params.id
+            }
+        });
+        if(!postData){
+            res.status(404).json({message: 'Post not found'})
+            return;
+        }
+        res.status(200).json({message: 'Post Deleted'},postData)
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
--- controllers/API/comments.js
+++ controllers/API/comments.js
@@ -61,3 +61,4 @@ router.delete('/:id', async (req,res) => {
         res.status(500).json(err);
     }
 });
+module.exports = router;
