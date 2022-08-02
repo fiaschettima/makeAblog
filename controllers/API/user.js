0b2057d12d9acf03f33b1815e60d79d7117385fb
Matt F <fiaschettima@gmail.com>
2022-08-02 00:28:16 -0400

    logout route created

--- controllers/API/user.js
+++ controllers/API/user.js
@@ -52,5 +52,11 @@ router.post('/login', async (req,res) =>{
 
 //Logout
 router.post('/logout',  (req,res) =>{
-
+    if(req.session.loggedIn){
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }else{
+        res.status(404).end();
+    }
 });
