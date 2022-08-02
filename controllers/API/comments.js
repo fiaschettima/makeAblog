43fbcac9662facfe54aad35c04a802d8e7924d99
Matt F <fiaschettima@gmail.com>
2022-08-02 01:55:57 -0400

    comment update and delete routes made

--- controllers/API/comments.js
+++ controllers/API/comments.js
@@ -2,30 +2,61 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+// get comments
+router.get('/', async (req,res) => {
+    try{
+        const commentData = await Comment.findAll();
+        if(!commentData){
+            res.status(404).json({message: 'No Comments for this post'});
+            return;
+        }
+        res.status(200).json(commentData);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 // add comment route
 router.post('/', async (req,res) => {
     try{
         const newComment = await Comment.create({
             content: req.body.content,
-            // user_id: ? get from somewhere besides body
+            user_id: req.session.user_id,
             //post_id: same here find where to pull id from
         });
+        res.status(200).json(newComment)
     }catch(err){
         res.status(500).json(err);
     }
 });
 // update comment route
-router.put('/', async (req,res) => {
+router.put('/:id', async (req,res) => {
     try{
-
+        const updateComment = await Comment.update(
+            {content: req.body.content},
+            {where:{id:req.params.id}}
+        );
+        if(!updateComment){
+            res.status(404).json({message: 'No matching comment'});
+            return;
+        }
+        res.status(200).json(updateComment);
     }catch(err){
         res.status(500).json(err);
     }
 });
 // delete comment route
-router.delete('/', async (req,res) => {
+router.delete('/:id', async (req,res) => {
     try{
-
+        const deletedComment = await Comment.destroy({
+            where:{
+                id: req.params.id
+            }
+        });
+        if(!deletedComment){
+            res.status(404).json({message: 'No comment found to delete'});
+            return;
+        };
+        res.status(200).json({message: 'Comment Deleted'},deletedComment)
     }catch(err){
         res.status(500).json(err);
     }
